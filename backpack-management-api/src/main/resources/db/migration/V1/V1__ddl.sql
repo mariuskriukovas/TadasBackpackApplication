0ca8c92dda023c8d2417b5da7da137b291361ff4
Marius <marius.kriukovas@gmail.com>
2022-08-27 15:24:15 +0300

    create simple db model;

--- backpack-management-api/src/main/resources/db/migration/V1/V1__ddl.sql
+++ backpack-management-api/src/main/resources/db/migration/V1/V1__ddl.sql
@@ -1,5 +1,42 @@
-CREATE TABLE test
+CREATE TABLE item
 (
-    id IDENTITY PRIMARY KEY,
-    name VARCHAR(50) NOT NULL
+    id                 IDENTITY PRIMARY KEY,
+    name               VARCHAR(100) NOT NULL,
+    is_mandatory       BOOLEAN      NULL,
+    weight             DOUBLE       NOT NULL,
+    provide_kilometers DOUBLE       NULL
 );
+
+CREATE TABLE traveler
+(
+    id   IDENTITY PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE travel
+(
+    id                  IDENTITY PRIMARY KEY,
+    name                VARCHAR(100) NOT NULL,
+    distance_kilometers DOUBLE       NOT NULL,
+    traveler_id         INT
+);
+
+ALTER TABLE travel
+    ADD FOREIGN KEY (traveler_id)
+        REFERENCES traveler (id);
+
+CREATE TABLE bag_item
+(
+    id        IDENTITY PRIMARY KEY,
+    travel_id INT,
+    item_id   INT
+);
+
+ALTER TABLE bag_item
+    ADD FOREIGN KEY (travel_id)
+        REFERENCES travel (id);
+
+
+ALTER TABLE bag_item
+    ADD FOREIGN KEY (item_id)
+        REFERENCES item (id);
--- backpack-management-api/src/main/resources/db/migration/V1/V2__insert_data.sql
+++ backpack-management-api/src/main/resources/db/migration/V1/V2__insert_data.sql
@@ -1,6 +1,11 @@
-INSERT INTO test (id, name)
-VALUES (1, 'testA');
-INSERT INTO test (id, name)
-VALUES (2, 'testB');
-INSERT INTO test (id, name)
-VALUES (3, 'testC');
+INSERT INTO PUBLIC.TRAVELER (ID, NAME)
+VALUES (1, 'Jonas'),
+       (2, 'Petras');
+
+INSERT INTO PUBLIC.ITEM (ID, NAME, IS_MANDATORY, WEIGHT, PROVIDE_KILOMETERS)
+VALUES (1, 'Vandens 1kg buteliukas', false, 1, 10),
+       (2, 'Palapinė', true, 6, NULL);
+
+INSERT INTO PUBLIC.TRAVEL (ID, NAME, DISTANCE_KILOMETERS, TRAVELER_ID)
+VALUES (1, 'Prie juros', 150, 1),
+       (2, 'Prie ežero', 20, 2);
