6a6e674540a05499cdcef9ced3b0be32ce299d46
Marius <marius.kriukovas@gmail.com>
2022-08-27 13:00:19 +0300

    init h2 db using flyway scripts;

---
+++ backpack-management-api/src/main/resources/db/migration/V1/V1__ddl.sql
@@ -0,0 +1,5 @@
+CREATE TABLE test
+(
+    id IDENTITY PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
---
+++ backpack-management-api/src/main/resources/db/migration/V1/V2__insert_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO test (id, name)
+VALUES (1, 'testA');
+INSERT INTO test (id, name)
+VALUES (2, 'testB');
+INSERT INTO test (id, name)
+VALUES (3, 'testC');
