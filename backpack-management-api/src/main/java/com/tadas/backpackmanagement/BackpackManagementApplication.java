4f8d4d2d77d4623435326abe9c019e259365c2c2
Marius <marius.kriukovas@gmail.com>
2022-08-27 11:25:49 +0300

    init spring boot application;

---
+++ backpack-management-api/src/main/java/com/tadas/backpackmanagement/BackpackManagementApplication.java
@@ -0,0 +1,13 @@
+package com.tadas.backpackmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackpackManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackpackManagementApplication.class, args);
+	}
+
+}
---
+++ backpack-management-api/src/test/java/com/tadas/backpackmanagement/BackpackManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tadas.backpackmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackpackManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
