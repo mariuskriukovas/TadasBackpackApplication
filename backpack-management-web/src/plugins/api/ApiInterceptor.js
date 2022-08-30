282bcaa40a7d920a12506356353b725735abd33b
Marius <marius.kriukovas@gmail.com>
2022-08-30 20:50:31 +0300

    fix api interceptor;

--- backpack-management-web/src/plugins/api/ApiInterceptor.js
+++ backpack-management-web/src/plugins/api/ApiInterceptor.js
@@ -1,45 +1,43 @@
 import axios from "axios";
-import isArray from "lodash-es/isArray";
+import qs from 'qs'
 
 export default () => {
-  const instance = axios.create({
-    baseURL: "/api",
-    withCredentials: false,
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-  });
-  instance.getPageableParams = function getPageableParams(options) {
-    let sortOrder = null;
-    if (options.sortDesc[0] === true) {
-      sortOrder = "desc";
-    } else if (options.sortDesc[0] === false) {
-      sortOrder = "asc";
-    }
-    const sort = [options.sortBy[0], sortOrder].filter(Boolean).join(",");
-    const params = { page: options.page - 1, size: options.itemsPerPage };
-    if (sort) {
-      params.sort = sort;
-    }
-    return { params };
-  };
-
-  instance.mapToFormData = function (form) {
-    const formData = new FormData();
-    Object.keys(form).forEach((key) => {
-      const val = form[key];
-      if (isArray(val)) {
-        Object.values(val).forEach((value) =>
-          value instanceof Blob
-            ? formData.append(key, value)
-            : formData.append(key, value)
-        );
-      } else if (val !== null && val !== undefined) {
-        formData.append(key, val);
-      }
+    const instance = axios.create({
+        baseURL: "/api", withCredentials: false, headers: {
+            Accept: "application/json", "Content-Type": "application/json",
+        },
     });
-    return formData;
-  };
-  return instance;
+    instance.getPageableParams = function getPageableParams(pageable) {
+        let sortOrder = null;
+        if (pageable.sortDesc[0] === true) {
+            sortOrder = "desc";
+        } else if (pageable.sortDesc[0] === false) {
+            sortOrder = "asc";
+        }
+        const sort = [pageable.sortBy[0], sortOrder].filter(Boolean).join(",");
+        const params = {page: pageable.page - 1, size: pageable.itemsPerPage};
+        if (sort) {
+            params.sort = sort;
+        }
+        return {params};
+    };
+
+    instance.getPageableAndFilterParams = function getPageableAndFilterParams(filters, pageable) {
+        const params = this.getPageableParams(pageable)
+        return {
+            params: Object.assign(params.params, filters), paramsSerializer: (p) => {
+                if (p === null) {
+                    return null
+                }
+                return qs.stringify(filterNonNull(p))
+            },
+        };
+    };
+
+    function filterNonNull(obj) {
+        // eslint-disable-next-line no-unused-vars
+        return Object.fromEntries(Object.entries(obj).filter(([k, v]) => v))
+    }
+
+    return instance;
 };
